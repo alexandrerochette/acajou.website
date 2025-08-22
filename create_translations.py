e1cf72fd24a5545323a89e9aa6cd1541367b0517
Alexandre Rochette <rochette@gmail.com>
2025-08-22 14:12:08 -0400

    add basic script to apply translations

---
+++ create_translations.py
@@ -0,0 +1,86 @@
+import os
+import glob
+import csv
+
+def find_all_files_recursive(directory_path, condition= lambda x: True):
+    """
+    Finds and returns a list of all file paths within a given directory
+    and its subdirectories.
+    """
+    all_files = []
+    for root, _, files in os.walk(directory_path):
+        for file in files:
+            if condition(file):
+
+              #  full_path = os.path.join(root, file)
+                all_files.append(file)
+    return all_files
+
+def get_translation_paths():
+    """
+        returns all translation mapping found from current config
+
+        Returns:
+            path to config mapping tsv file
+    """
+    result = []
+    for file in glob.glob("translations/*.tsv"):
+        result.append(file)
+
+    return result
+
+def locale_from_path(path):
+    filename_with_ext = os.path.basename(path)
+    filename_without_ext = os.path.splitext(filename_with_ext)[0]
+    return  filename_without_ext
+
+def load_translation_mapping(path):
+    result = {}
+    with open(path) as inputf:
+        reader = csv.reader(inputf, delimiter="\t")
+        for row in reader:
+            if len(row) > 1:
+                result[row[0]] = row[1]
+    return result
+
+def try_make_dir(path):
+    try:
+        print(path)
+        os.makedirs(path)
+    except FileExistsError:
+        pass
+
+def create_translations():
+    root_path = 'source'
+    output_path = 'docs'
+    localization_paths = get_translation_paths()
+    for path in localization_paths:
+        locale = locale_from_path(path)
+        locale_path = os.path.join(output_path, locale)
+        try_make_dir(locale_path)
+
+
+
+    all_source_files = find_all_files_recursive(root_path)
+    for source_file in all_source_files:
+        with open(os.path.join(root_path, source_file), "rb") as inputf, open(os.path.join(output_path, source_file), "wb") as outf:
+            outf.write(inputf.read())
+        if source_file.endswith('.html'):   
+            
+                for path in localization_paths:
+                    locale = locale_from_path(path)
+                    mappings = load_translation_mapping(path)
+                    locale_path = os.path.join(root_path, locale)
+            
+                    with open(os.path.join(root_path, source_file), "r", encoding="utf-8") as inputf, open(os.path.join(output_path, locale, source_file), "w", encoding="utf-8") as outf:
+                        all_input_text = inputf.read()
+                   
+                        for mapping_key in sorted(mappings, key= len, reverse=True):
+                            source_text = mapping_key
+                            translated_text = mappings[mapping_key]
+                            print(source_text, '--', translated_text)
+                            all_input_text = all_input_text.replace(source_text, translated_text)
+                        outf.write(all_input_text)
+
+create_translations()
+
